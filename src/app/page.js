03a6b63bad4785115a1c4462b82343bb7c16fa3f
yashin <yashinalfauzysabara@gmail.com>
2024-06-20 15:51:25 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -19,13 +19,14 @@ export default function Home() {
         <link rel="icon" href="/icon.jpg"></link>
       </Head>
       <main className="px-5 bg-gray-50 dark:bg-gray-900 dark:text-white font-poppins">
-        <nav className="pb-4 pt-2 lg:py-4 lg:px-3 rounded-md mb-5 mx-2 flex flex-col lg:flex-row justify-between sticky top-2 items-center bg-gray-50 dark:bg-gray-800 shadow-lg z-10">
+        <nav className="pb-4 pt-2 lg:py-4 lg:px-3 rounded-md mb-5 lg:mx-2 flex flex-col lg:flex-row justify-between sticky top-2 items-center bg-gray-50 dark:bg-gray-800 shadow-lg z-10">
           <h1 className="text-base lg:text-xl mx-auto lg:mx-0 py-2 dark:text-white">#developedbyyyacyn</h1>
-          <ul className="flex items-center gap-3 lg:gap-5 text-xs lg:text-xl justify-around px-2">
+          <ul className="flex items-center gap-2 lg:gap-5 text-xs lg:text-xl justify-around px-2">
             <li className="text-blue-800 hover:bg-blue-800 hover:text-white dark:text-white p-0 lg:px-2 lg:py-1 rounded-md transition-all active:bg-blue-600"><a href="#about">About Me</a></li>
             <li className="text-blue-800 hover:bg-blue-800 hover:text-white p-0 dark:text-white lg:px-2 lg:py-1 rounded-md transition-all active:bg-blue-600"><a href="#skills">Skills</a></li>
             <li className="text-blue-800 hover:bg-blue-800 hover:text-white p-0 dark:text-white lg:px-2 lg:py-1 rounded-md transition-all active:bg-blue-600"><a href="#projects">Projects</a></li>
-            <li className="ml-10"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer lg:text-2xl transition-all hover:scale-105 active:scale-95 dark:text-white" /></li>
+            <li className="text-blue-800 hover:bg-blue-800 hover:text-white p-0 dark:text-white lg:px-2 lg:py-1 rounded-md transition-all active:bg-blue-600"><a href="#contact">Contact</a></li>
+            <li className="lg:ml-10"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer lg:text-2xl transition-all hover:scale-105 active:scale-95 dark:text-white" /></li>
             <li><a className="hover:bg-blue-700 bg-blue-800  text-white p-2 px-2 rounded-md lg:text-xl" href="#">Resume</a>
             </li>
           </ul>
